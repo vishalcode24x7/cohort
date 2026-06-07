245e2af93a5b10e3c6d2386bd5cf29631438c3a6
vishal <vishalcode247@gmail.com>
2026-06-07 22:31:16 +0530

    loop

--- javascript/script.js
+++ javascript/script.js
@@ -1,62 +1,27 @@
-// console.info("hello")
-// console.log("hi")
-// console.warn("yo")
-// console.error("kya hal hai")
-// console.table({Name: "vishal",class:10})
-
-// alert("ERROR")
-// confirm("batao")
-// prompt("likho")
-
-// let word = "kaise ho bhai"
-// console.log(word.slice(4,10))
-// console.log(`vishal ${2+2}`)
-// console.log(word.split('h'))
-// console.log(word.replace("ho","tha"))
-// console.log(word.includes('h'))  
-
-// console.time("loopTime");
-
-
-// console.group("user info")
-// console.log("Name: vishal")
-// console.log("course: js")
-// console.groupEnd();
-
-//data types
-// float
-// number
-// null
-// Boolean
-// long
-// Array
-// String
-// undefined
-// Symbol
-// Object
-// NaN
-
-// reference - array objects function
-// primitives - Number Boolean String null undefined NaN Infinity Symbol BigInt
-
-// {
-//   var a = 5;
+//for loop
+//  for(let i = 1; i<=10; i++){
+//     console.log(i)
+//  }
+// for(let i = 1;i<10;i++){
+//    if(i%2 === 0){
+//       console.log(i)
+//    }
 // }
 
-// console.log(a)
+for(let i = 10; i>=1; i--){
+   console.log(i)
+}
 
-// [Operators]
+for(let i=1; i<=5; i++){
+   console.log("yes")
+}
 
-// arithmetic Operators
-// assignment Operators
-// comparison Operators
-// logical Operatorsn 
-// condition (Ternary) Operator
-// Type Checing Operators
-// String Operator
-// Spread / Rest Operator
-// Nullish Coalescing Operator
-// optional Operator
- 
-
-// [variable hoisting]
+let num = prompt("Enter number");
+if (age === null){
+   console.error("You Cancelled")
+}
+else{
+   if(age.trim() === ""){
+      console.error
+   }
+}
