c51862d4f85449f4b592d57892fe636983587b5c
vishal <vishalcode247@gmail.com>
2026-06-14 18:39:16 +0530

    second task done

--- javascript/script.js
+++ javascript/script.js
@@ -2,19 +2,20 @@ let btn = document.querySelector("button");
 let main = document.querySelector("main");
 
 btn.addEventListener("click", function () {
-    let div = document.createElement("div");
-    let x = Math.random()*100;
-    let y = Math.random()*100;
-    var c1 = Math.floor(Math.random()*256);
-    var c2 = Math.floor(Math.random()*256);
-    var c3 = Math.floor(Math.random()*256);
+    let arr = ["Hello world", "how are you", "looking good !!!", "have a nice day", "keep smiling", "you are amazing", "javascript is fun", "coding is life", "never give up", "believe in yourself"];
+    let x = Math.floor(Math.random() * 80);
+    let y = Math.floor(Math.random() * 80);
+    let rotate = Math.floor(Math.random() * 360);
+    let scale = Math.random() * 2 + 0.5;
+    let a = Math.floor(Math.random() * arr.length);
 
-    div.style.width = "100px";
-    div.style.height = "100px";
-    div.style.position = "absolute";
-    div.style.left = x + "%";
-    div.style.top = y + "%";
-    div.style.border = "1px solid black";
-    div.style.backgroundColor = `rgb(${c1}, ${c2}, ${c3})`;
-    main.appendChild(div);
+
+    let h1 = document.createElement("h1");
+    h1.textContent = arr[a];
+    h1.style.position = "absolute";
+    h1.style.left = x + "%";
+    h1.style.top = y + "%";
+    h1.style.transform = "rotate(" + rotate + "deg)";
+    h1.style.scale = scale;
+    main.appendChild(h1);
 })
