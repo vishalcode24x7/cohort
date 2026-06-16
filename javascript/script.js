162802317bb9a76bcf8232533250b1746b044f60
vishal <vishalcode247@gmail.com>
2026-06-16 11:21:55 +0530

    insta like button done

--- javascript/script.js
+++ javascript/script.js
@@ -1,21 +1,16 @@
-let btn = document.querySelector("button");
-let main = document.querySelector("main");
+let card = document.querySelector(".card");
+let love = document.querySelector("#love");
 
-btn.addEventListener("click", function () {
-    let arr = ["Hello world", "how are you", "looking good !!!", "have a nice day", "keep smiling", "you are amazing", "javascript is fun", "coding is life", "never give up", "believe in yourself"];
-    let x = Math.floor(Math.random() * 80);
-    let y = Math.floor(Math.random() * 80);
-    let rotate = Math.floor(Math.random() * 360);
-    let scale = Math.random() * 2 + 0.5;
-    let a = Math.floor(Math.random() * arr.length);
-
-
-    let h1 = document.createElement("h1");
-    h1.textContent = arr[a];
-    h1.style.position = "absolute";
-    h1.style.left = x + "%";
-    h1.style.top = y + "%";
-    h1.style.transform = "rotate(" + rotate + "deg)";
-    h1.style.scale = scale;
-    main.appendChild(h1);
+card.addEventListener("dblclick", function () {
+    love.style.opacity = 1;
+    love.style.transform = 'translate(-50%, -50%) scale(1) rotate(0deg)'
+    
+    setTimeout(function() {
+        love.style.opacity = 0;
+        love.style.transform = 'translate(-50%, -200%) scale(1) rotate(60deg)'
+    }, 800);
+    setTimeout(function() {
+        love.style.opacity = 0;
+        love.style.transform = 'translate(-50%, -50%) scale(0) rotate(60deg)'
+    }, 1000);
 })
