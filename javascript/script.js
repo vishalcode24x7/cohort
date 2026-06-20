f7d3d7c3ca3a586e7c989c88bf39ddd10ce4e236
vishal <vishalcode247@gmail.com>
2026-06-20 17:51:55 +0530

    matrix text done

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,33 @@
-addEventListener("mousemove", (e)=>{
-    document.body.style.setProperty("--x", e.clientX + 'px')
-    document.body.style.setProperty("--y", e.clientY + 'px')
-})
+const paragraphs = document.querySelectorAll("p");
+const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+paragraphs.forEach((para) => {
+    const text = para.innerText;
+    let iteration = 0;
+    let interval;
+
+    function randomText() {
+        const str = text.split('').map((char, index) => {
+            if (index < iteration) {
+                return char;
+            }
+
+            return characters[Math.floor(Math.random() * 52)];
+        }).join("");
+
+        para.innerText = str;
+
+        iteration += 0.4;
+
+        if (iteration >= text.length) {
+            clearInterval(interval);
+            para.innerText = text;
+        }
+    }
+
+    para.addEventListener("mouseenter", () => {
+        iteration = 0;
+        clearInterval(interval);
+        interval = setInterval(randomText, 30);
+    });
+});
