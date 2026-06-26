29ad2d8fe49df01c16aa84619ec0e224215123eb
vishal <vishalcode247@gmail.com>
2026-06-26 19:25:44 +0530

    OOPs starts

--- javascript/script.js
+++ javascript/script.js
@@ -1,33 +1,8 @@
-const paragraphs = document.querySelectorAll("p");
-const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-
-paragraphs.forEach((para) => {
-    const text = para.innerText;
-    let iteration = 0;
-    let interval;
-
-    function randomText() {
-        const str = text.split('').map((char, index) => {
-            if (index < iteration) {
-                return char;
-            }
-
-            return characters[Math.floor(Math.random() * 52)];
-        }).join("");
-
-        para.innerText = str;
-
-        iteration += 0.4;
-
-        if (iteration >= text.length) {
-            clearInterval(interval);
-            para.innerText = text;
-        }
+class Biscutmaker{
+    constructor(){
+        this.name = "parleG";
+        this.price = 5
     }
+}
 
-    para.addEventListener("mouseenter", () => {
-        iteration = 0;
-        clearInterval(interval);
-        interval = setInterval(randomText, 30);
-    });
-});
+let biscut1 = new Biscutmaker();
