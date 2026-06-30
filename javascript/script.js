a1b0d3f0cb0077a0ef509c0c4874ca65204ba97a
vishal <vishalcode247@gmail.com>
2026-06-30 11:39:16 +0530

    callbacks question done

--- javascript/script.js
+++ javascript/script.js
@@ -1,19 +1,57 @@
-function a(fn){
-    fn(function(fn3){
-        fn3(function(fn5){
-            fn5(function(fn7){
-                fn7();
-            });
-        });
-    });
+//Quetions on callbacks
+// Q1
+// function afterdelay(time, cb){
+//     setTimeout(() => {
+//         cb();
+//     }, time);
+// }
+// afterdelay(3000, function(){
+//     console.log("executed")
+// })
+
+// Q2
+// function getUser(username, cb){
+//     console.log("getting user details....");
+//     setTimeout(() => {
+//         cb({id:1, username: "vishal"});
+//     }, 1000);
+// }
+// function getUserPosts(id, cb){
+//     console.log("getting users posts....")
+//     setTimeout(() => {
+//         cb(["hello", "good day", "yoo"])
+//     }, 2000);
+// }
+// getUser("vishal", function(data){
+//     getUserPosts(data.id, function(allposts){
+//         console.log(data.username, allposts)
+//     })
+// })
+
+// Q3
+function loginUser(username, cb){
+    console.log("logging in user....")
+    setTimeout(() => {
+        cb({id:1212, username:"vihal"});
+    }, 1000);
+}
+function fetchPermissions(id, cb){
+    console.log("loading permissions....")
+    setTimeout(() => {
+        cb(["read","write","delete"])
+    }, 2000);
+}
+function loadDashboard(permission, cb){
+    console.log("loggging dashboard....");
+    setTimeout(() => {
+        cb();
+    }, 2000);
 }
 
-a(function(fn2){
-    fn2(function(fn4){
-        fn4(function(fn6){
-            fn6(function(){
-                console.log("callbacks")
-            });
-        });
-    });
-});
+loginUser("vishal", function(userdata){
+    fetchPermissions(userdata.id, function(permissions){
+        loadDashboard(permissions, function(){
+            console.log("dashboard loaded ✅");
+        })
+    })
+})
