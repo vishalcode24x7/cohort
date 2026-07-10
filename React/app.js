93291dd41767e2494a7a1d27e1bbbe23b9b002d7
vishal <vishalcode247@gmail.com>
2026-07-10 15:18:48 +0530

    start react

---
+++ React/app.js
@@ -0,0 +1,5 @@
+import app from './main.js'
+
+const root = ReactDom.createRoot(document.querySelector('#container'))
+
+root.render(app())
---
+++ React/main.js
@@ -0,0 +1,5 @@
+const app = ()=>{
+    return 'gthis klsdjflk'
+}
+
+export default app
