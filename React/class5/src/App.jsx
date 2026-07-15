072ddba6ee79f544d750d42cedbf12837e88a318
vishal <vishalcode247@gmail.com>
2026-07-15 14:31:23 +0530

    one more task of useState

--- React/class5/src/App.jsx
+++ React/class5/src/App.jsx
@@ -1,22 +1,27 @@
 import React from "react";
+import { useState } from "react";
 
 const App = () => {
-  const [num, setNum] = useState(0);
+  const [marks, setMarks] = useState([54, 65, 76, 32, 11]);
+  function graceStudent() {
+    const newmarks = marks.map(function (elem) {
+      return elem + 5;
+    });
+    setMarks(newmarks)
+    console.log(newmarks)
+  }
 
   return (
-    <div>
-      <div className="box">
-        {num}
-        
-      </div>
-      <button
-        onClick={() => {
-          const rdm = Math.floor(Math.random() * 100);
-          setNum(rdm);
-        }}
-      >
-        click here
-      </button>
+    <div className="main">
+      {marks.map(function (elem, idx) {
+        return (
+          <h1 key={idx}>
+            student {idx+1} = {elem}
+             ({elem>33?'PASS':'FAIL'})
+          </h1>
+        );
+      })}
+      <button onClick={graceStudent}>Give them grace</button>
     </div>
   );
 };
