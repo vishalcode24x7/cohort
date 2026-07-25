7ee160cd520b74042115cda00671c9e3c1628ec3
vishal <vishalcode247@gmail.com>
2026-07-25 09:55:37 +0530

    class 9 project

---
+++ React/class9_project/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import axios from 'axios'
+
+const App = () => {
+  async function callapi(){
+    console.log('api calling')
+    let response = await axios.get("https://pokeapi.co/api/v2/pokemon/10");
+    console.log(response)
+
+  }
+  return (
+    <div>
+      <button onClick={callapi}>Click it</button>
+    </div>
+  )
+}
+
+export default App
+-/
