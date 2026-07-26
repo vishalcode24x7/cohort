87c8703855793d569a337a076eaa9768f8760e07
vishal <vishalcode247@gmail.com>
2026-07-26 21:35:20 +0530

    classs 11 continue

---
+++ React/class11/src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='h-screen bg-black text-white text-3xl '>
+      About
+    </div>
+  )
+}
+
+export default About
