d18101eaf3396ab48c627486b9520b6c5af40ee6
vishal <vishalcode247@gmail.com>
2026-07-30 21:47:27 +0530

    mouse follower done

---
+++ javascript/app.js
@@ -0,0 +1,18 @@
+const mouseFollower = document.querySelector(".mouse-follower")
+
+let x = 0;
+let y = 0;
+
+addEventListener("mousemove",(e)=>{
+    const {clientX, clientY} = e
+    x = clientX
+    y = clientY
+
+})
+
+function far(){
+    mouseFollower.style.transform = `translate(${x}px,${y}px)`
+    requestAnimationFrame(far)
+    
+}
+far()
