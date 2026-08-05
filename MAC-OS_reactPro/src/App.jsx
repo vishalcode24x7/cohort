4cfeb6c4f0cebd88327a7b2ae04595b8483f1ef0
vishal <vishalcode247@gmail.com>
2026-08-05 21:21:25 +0530

    dragable window done

--- MAC-OS_reactPro/src/App.jsx
+++ MAC-OS_reactPro/src/App.jsx
@@ -3,15 +3,14 @@ import "./app.scss"
 import Dock from './components/Dock'
 import Nav from './components/Nav'
 import MacWindow from './components/windows/MacWindow'
+import Github from './components/windows/Github'
 
 const App = () => {
   return (
     <main>
       <Nav />
       <Dock />
-      <MacWindow>
-        <h1>hello</h1>
-      </MacWindow>
+      <Github />
     </main>
   )
 }
--- MAC-OS_reactPro/src/components/windows/MacWindow.jsx
+++ MAC-OS_reactPro/src/components/windows/MacWindow.jsx
@@ -4,7 +4,14 @@ import './window.scss'
 
 const MacWindow = ({children}) => {
   return (
-    <Rnd>
+    <Rnd
+    default={{
+      width:"40vw",
+      height:"40vh",
+      x:300,
+      y:200
+    }}
+    >
       <div className="window">
         <div className="nav">
           <div className="dots">
