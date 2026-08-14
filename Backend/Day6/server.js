8b0592f66236092c01092bfdef06974cfb2ca6d2
vishal <vishalcode247@gmail.com>
2026-08-14 22:15:43 +0530

    done

---
+++ Backend/Day6/server.js
@@ -0,0 +1,17 @@
+
+
+const app = require("./src/app")
+const mongoose = require("mongoose")
+
+function connectToDb(){
+    mongoose.connect("mongodb+srv://lxvishaaallll_db_user:HPmyOshsCIKgMJDx@cluster0.xm9jugc.mongodb.net/day-6")
+    .then(()=>{
+        console.log("connected to Database")
+    }) 
+}
+
+connectToDb()
+ 
+app.listen(3000, ()=>{
+    console.log("server is running on 3000");
+})
