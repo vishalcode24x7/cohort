5c8b92575b3fea1d766de4564100d8673550c3cd
vishal <vishalcode247@gmail.com>
2026-08-15 23:05:43 +0530

    adding comments

--- Backend/Day6/server.js
+++ Backend/Day6/server.js
@@ -3,13 +3,15 @@
 const app = require("./src/app")
 const mongoose = require("mongoose")
 
+//function
 function connectToDb(){
     mongoose.connect("mongodb+srv://lxvishaaallll_db_user:HPmyOshsCIKgMJDx@cluster0.xm9jugc.mongodb.net/day-6")
     .then(()=>{
         console.log("connected to Database")
     }) 
-}
+     
 
+}
 connectToDb()
  
 app.listen(3000, ()=>{
