1c6bfca93b0510cf67f87437627e20a7b8c42a9b
vishal <vishalcode247@gmail.com>
2026-08-26 22:27:48 +0530

    day 12 done

---
+++ Backend/day12/src/routes/auth.routes.js
@@ -0,0 +1,37 @@
+const express = require("express")
+const userModel = require("../models/user.model")
+const jwt = require("jsonwebtoken")
+const authRouter = express.Router()
+
+authRouter.post("/register", async (req, res) => {
+    const { email, name, password } = req.body
+
+    const isUserAlreadyExists = await userModel.findOne({ email })
+    if (isUserAlreadyExists) {
+        return res.status(409).json({
+            message: "user already exists with this email"
+        })
+    }
+
+    const user = await userModel.create({
+        email, password, name
+    })
+
+    const token = jwt.sign(
+        {
+            id: user._id,
+            email: user.email
+        },
+        process.env.JWT_SECRET
+    )
+
+    res.cookie("jwt_token", token)
+
+    res.status(201).json({
+        message: "user created",
+        user,
+        token
+    })
+})
+
+module.exports = authRouter
