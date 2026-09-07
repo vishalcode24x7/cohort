2a65ebb13b12af1b3c21e2c058422aed8ae8af0d
vishal <vishalcode247@gmail.com>
2026-09-07 22:09:34 +0530

    making 2 more api

--- Backend/Day15/src/controllers/post.controller.js
+++ Backend/Day15/src/controllers/post.controller.js
@@ -20,21 +20,21 @@ async function createPostController(req, res) {
 
     let decoded = null;
 
-    try{
+    try {
         decoded = jwt.verify(token, process.env.JWT_SECRET)
-    }catch(err){
+    } catch (err) {
         return res.status(401).json({
             message: "user not authorized"
         })
     }
-    
+
     console.log(decoded)
 
 
     const file = await imagekit.files.upload({
         file: await toFile(Buffer.from(req.file.buffer), 'file'),
         fileName: "Test",
-        folder:"instagram-clone"
+        folder: "instagram-clone"
     })
 
     const post = await postModel.create({
@@ -49,8 +49,76 @@ async function createPostController(req, res) {
     })
 }
 
+async function getPostController(req, res) {
+    const token = req.cookies.token
+    if (!token) {
+        return res.status(401).json({
+            message: "Unauthorized Access"
+        })
+    }
 
+    let decoded = null;
+    try {
+        decoded = jwt.verify(token, process.env.JWT_SECRET)
+    } catch (err) {
+        return res.status(401).json({
+            message: "Token invalid"
+        })
+    }
+    const userId = decoded.id
+    const posts = await postModel.find({
+        user: userId
+    })
+
+    res.status(200).json({
+        message: "post fetched successfully",
+        posts
+    })
+}
+
+async function getPostDetailsController(req, res) {
+    const token = req.cookies.token
+
+    if (!token) {
+        return res.status(401).json({
+            message: "Unauthorized Access"
+        })
+    }
+
+    let decoded = null;
+    try {
+        decoded = jwt.verify(token, process.env.JWT_SECRET)
+    } catch (err) {
+        return res.status(401).json({
+            message: "Invalid Token"
+        })
+    }
+
+    const userId = decoded.id
+    const postId = req.params.postId
+
+    const post = await postModel.findById(postId)
+    if (!post) {
+        return res.status(404).json({
+            message: "Post not found."
+        })
+    }
+
+    const isValidUser = post.user.toString() === userId
+    if (!isValidUser) {
+        return res.status(403).json({
+            message: "Forbidden content"
+        })
+    }
+
+    return res.status(200).json({
+        message:"Post fetched successfully.",
+        post
+    })
+}
 
 module.exports = {
-    createPostController
+    createPostController,
+    getPostController,
+    getPostDetailsController
 }  
--- Backend/Day15/src/routes/post.routes.js
+++ Backend/Day15/src/routes/post.routes.js
@@ -2,8 +2,12 @@ const express = require('express')
 const postRouter = express.Router()
 const postController = require('../controllers/post.controller')
 const multer = require('multer')
-const upload = multer({storage: multer.memoryStorage()})
+const upload = multer({ storage: multer.memoryStorage() })
 
-postRouter.post('/', upload.single("image") , postController.createPostController)
+postRouter.post('/', upload.single("image"), postController.createPostController)
+
+postRouter.get('/', postController.getPostController)
+
+postRouter.get('/details/:postId', postController.getPostDetailsController)
 
 module.exports = postRouter
