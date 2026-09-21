556c4f0212d3709a486f223a191d4bee14aacbee
vishal <vishalcode247@gmail.com>
2026-09-21 06:49:09 +0530

    login regiseter done

--- Backend/instagram-project/Frontend/src/features/auth/pages/Register.jsx
+++ Backend/instagram-project/Frontend/src/features/auth/pages/Register.jsx
@@ -1,21 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router'
+import axios from 'axios'
 
 
 const Register = () => {
+
+
+  const [username, setUsername] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  async function handleSubmit(e){
+    e.preventDefault()
+
+    axios.post("http://localhost:3000/api/auth/register", {
+      username,
+      email,
+      password
+    },{
+      withCredentials: true
+    })
+    .then(res => {
+      console.log(res.data)
+    })
+  }
+
   return (
     <main>
-        <div className="form-container">
-            <h1>Register</h1>
-            <form>
-                <input type="text" name='username' placeholder='Enter username' />
-                <input type="email" name='email' placeholder='Enter email' />
-                <input type="password" name='passoword' placeholder='Enter password' />
-                <button>Register</button>
-            </form>
+      <div className="form-container">
+        <h1>Register</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            onChange={(e) => { setUsername(e.target.value) }}
+            type="text"
+            name='username'
+            placeholder='Enter username' />
+          <input
+            onChange={(e) => { setEmail(e.target.value) }}
+            type="email" 
+            name='email' 
+            placeholder='Enter email' />
+          <input
+            onChange={(e) => { setPassword(e.target.value) }}
+            type="password" 
+            name='passoword' 
+            placeholder='Enter password' />
+          <button>Register</button>
+        </form>
 
-            <p>Already have an account? <Link className='toggleAuthForm' to="/login"> Login </Link></p>
-        </div>
+        <p>Already have an account? <Link className='toggleAuthForm' to="/login"> Login </Link></p>
+      </div>
     </main>
   )
 }
