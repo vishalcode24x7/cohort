2a1d58639d581be45e7634ecffb6b44a978da72c
vishal <vishalcode247@gmail.com>
2026-09-24 08:59:19 +0530

    implemented like feature

--- Backend/instagram-project/Backend/src/controllers/post.controller.js
+++ Backend/instagram-project/Backend/src/controllers/post.controller.js
@@ -101,7 +101,22 @@ async function likePostController(req, res){
 
 
 async function getFeedController(req, res){
-    const posts = await postModel.find().populate("user").select("-user.password")
+
+    const user = req.user
+
+    const posts = await Promise.all((await postModel.find().populate("user").lean())
+                    .map(async (post)=> {
+                        console.log(typeof post);
+                        
+                        const isLiked = await likeModel.findOne({
+                            user: user.username,
+                            post: post._id
+                        })
+
+                        post.isLiked = Boolean(isLiked)
+
+                        return post
+                    }))
 
     res.status(200).json({
         message: "Posts fetched successfully",
--- Backend/instagram-project/Frontend/src/features/post/pages/Feed.jsx
+++ Backend/instagram-project/Frontend/src/features/post/pages/Feed.jsx
@@ -24,7 +24,9 @@ const Feed = () => {
         <main className='feed-page'>
             <div className="feed">
                 <div className="posts">
-                    {/* <Post /> */}
+                    {feed.map(post=>{
+                        return <Post user={post.user} post={post} />
+                    })}
                 </div>
             </div>
         </main>
